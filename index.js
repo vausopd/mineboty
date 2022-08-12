d184cf4ff4054763a905ade292b52270344c3fa4
vausopd <110972438+vausopd@users.noreply.github.com>
2022-08-12 16:09:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const {mineboty} = require("mineboty")
-mineboty();
+mineboty();//It can be use also in TypeScript same which is
+ const {mineboty} = require("mineboty")
+ //no change in main file :)
+ mineboty(); 
+ //This much only now make config.json
